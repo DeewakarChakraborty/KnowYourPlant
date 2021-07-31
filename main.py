c1ab1e241f184d7d980aa0934f1725614acbf601
Deewakar Chakraborty <dc6shibun@gmail.com>
2021-07-31 18:11:37 +0530

    Final Commit

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.uix.textinput import TextInput
 import requests 
+from plyer import filechooser
 
 class FirstWindow(Screen):
     pass
@@ -19,29 +20,31 @@ class AboutWindow(Screen):
     pass
 
 class FlowerPredictionWindow(Screen):
-    def selected(self, filename):
+    def selected(self):
         try:
-            self.ids.my_image.source = filename[0]
+            file_path = filechooser.choose_dir(title = "Choose jpg/png", filters=[("jpg", "*png")])
+            self.ids.my_image.source = file_path[0]
             #print(filename[0])
-            resp = requests.post("https://pytorch-flower-classifier.herokuapp.com/predict", files={'file': open(filename[0], 'rb')})
+            resp = requests.post("https://pytorch-flower-classifier.herokuapp.com/predict", files={'file': open(file_path[0], 'rb')})
             str= resp.text.split(',')[0].split(':')[1][1:-1]
             self.ids.xyz.text = str
 
         except:
-            pass
+            print("Exception block")
        
 
 class LeafPredictionWindow(Screen):
-    def selected(self, filename):
+    def selected(self):
         try:
-            self.ids.my_image.source = filename[0]
+            file_path = filechooser.choose_dir(on_selection=self.handle_selection)
+            self.ids.my_image.source = file_path[0]
             #print(filename[0])
-            resp = requests.post("https://leaf-disease-classifier.herokuapp.com/predict", files={'file': open(filename[0], 'rb')})
+            resp = requests.post("https://leaf-disease-classifier.herokuapp.com/predict", files={'file': open(file_path[0], 'rb')})
             str = resp.text.split(',')[0].split(':')[1][1:-1]
             self.ids.abc.text = str
 
         except:
-            pass
+            print("Exception block")
 
 class WindowManager(ScreenManager):
     pass
